1a1d38ec84e621e0bcf493644218f52c01e75c5b
hernangonzalez <hernan@indeba.com>
2020-01-10 19:40:08 +0100

    Initial Commit

---
+++ FQMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FQMap
+//
+//  Created by Hernan G. Gonzalez on 10/01/2020.
+//  Copyright © 2020 Hernan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
